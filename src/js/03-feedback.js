217ae6157666009fbf0ab3c782e3c4ad969d3164
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-10 14:45:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,41 +5,32 @@ const emailEl = document.querySelector('input[name="email"]');
 const messageEl = document.querySelector('textarea[name="message"]');
 
 const STORAGE_KEY = 'feedback-form-state';
-const dataLocalStorage = localStorage.getItem(STORAGE_KEY);
 
-const dataObj = {};
+const dataObj = {
+  email: '',
+  message: '',
+};
+
+const dataLocalStorage =
+  localStorage.getItem(STORAGE_KEY) || JSON.stringify(dataObj);
 let dataParse;
 
 getDataFromLocalStorage();
 
 function getDataFromLocalStorage() {
-  if (dataLocalStorage && dataLocalStorage !== '') {
-    try {
-      dataParse = JSON.parse(dataLocalStorage);
-      dataObj.email = dataParse.email;
-      dataObj.message = dataParse.message;
-      emailEl.value = dataParse.email || '';
-      messageEl.value = dataParse.message || '';
-    } catch (error) {
-      console.log('error', error);
-    }
-  }
+  dataParse = JSON.parse(dataLocalStorage);
+  dataObj.email = dataParse.email;
+  dataObj.message = dataParse.message;
+  emailEl.value = dataParse.email || '';
+  messageEl.value = dataParse.message || '';
 }
 
 function handleChangeForm(e) {
   e.preventDefault();
   const { name, value } = e.target;
   dataObj[name] = value;
-
-  if (Object.keys(dataObj).length > 0) {
-    let data;
-    try {
-      data = JSON.stringify(dataObj);
-      localStorage.setItem(STORAGE_KEY, data);
-    } catch (error) {
-      console.log(error, 'error');
-    }
-  }
+  console.log('dataObj  handleCh', dataObj);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
 }
 
 function handleSubmitForm(e) {
